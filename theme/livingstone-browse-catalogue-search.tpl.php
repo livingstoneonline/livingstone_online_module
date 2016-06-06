7de1e4a4d296fa41d50c1274c9cdc664f0609538
Nigel Banks <nigel.g.banks@gmail.com>
2016-06-06 15:59:20 +0100

    Catalogue display, search bar mobile support

--- theme/livingstone-browse-catalogue-search.tpl.php
+++ theme/livingstone-browse-catalogue-search.tpl.php
@@ -7,7 +7,7 @@
 <table>
   <tbody>
     <tr>
-      <td style="text-align: left; width: 75%;"><?php print drupal_render($element['query']); ?></td>
+      <td style="text-align: left; width: 100%;"><?php print drupal_render($element['query']); ?></td>
       <td style="text-align: right; width: 25%;"><?php print drupal_render($element['search']); ?></td>
     </tr>
   </tbody>
