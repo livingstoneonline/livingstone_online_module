cc8b26a64851861c67e2e32751def8e0fcad9bfc
Adrian Wisnicki <awisnicki@yahoo.com>
2016-06-20 12:56:09 -0500

    Change to restricted view text

--- theme/livingstone-manuscript-viewer.tpl.php
+++ theme/livingstone-manuscript-viewer.tpl.php
@@ -53,9 +53,9 @@
         <h1>Restricted Item</h1>
         <hr/>
         <p>
-          Livingstone Online has images of the item you have selected, but we do
-          not have permission to post them online or otherwise share them.
-          Time permitting, we may be able to consult the item for individuals with
+          Livingstone Online has images of the item you have selected, but we are either still preparing them for publication or do
+          not have permission to post them online.
+          Time permitting, we may be able to consult the images for individuals with
           legitimate research needs or questions. Please
           <a href="mailto:awisnicki@yahoo.com">email us</a> to inquire further.
         </p>
